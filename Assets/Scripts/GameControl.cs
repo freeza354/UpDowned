3f57aeba445a87614f1e2066c0f12216f46a8f04
freeza354 email = freeza354@gmail.com <freeza354@gmail.com>
2018-10-30 01:17:28 -0700

    Upload Game

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+
+    public GameObject Enemy;
+
+    private bool isSpawning = true;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("space"))
+        {
+
+            Debug.Log("This code is running");
+
+            if (MovingObstacle.turnOver)
+            {
+                MovingObstacle.turnOver = false;
+            }
+            else
+            {
+                MovingObstacle.turnOver = true;
+            }
+        }
+
+        if (isSpawning)
+        {
+            isSpawning = false;
+            StartCoroutine(spawnEnemy());            
+        }
+
+    }
+
+    IEnumerator spawnEnemy()
+    {
+        yield return new WaitForSeconds(5f);
+        isSpawning = true;
+        Instantiate(Enemy);
+    }
+
+}
---
+++ Assets/Scripts/MovingObstacle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObstacle : MonoBehaviour {
+
+    public static bool turnOver = true;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (turnOver)
+        {
+            transform.Translate(new Vector2(-3f, -1) * Time.deltaTime);
+        }
+        else if (turnOver == false)
+        {
+            transform.Translate(new Vector2(-3f, 1) * Time.deltaTime);
+        }
+
+        if (transform.position.x <= -24f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
